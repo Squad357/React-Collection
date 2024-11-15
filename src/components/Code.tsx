b161a29b052b6ee5b430793be6c6bb289878307c
Moong <tmdals9154@gmail.com>
2024-11-15 11:59:12 +0900

    ✨ [Feat] shiki 라이브러리 도입 및 Code 컴포넌트 분리 (FS-25)

---
+++ src/components/Code.tsx
@@ -0,0 +1,56 @@
+// Code.tsx
+import { useEffect, useState } from 'react';
+import { codeToHtml } from 'shiki';
+
+interface CodeProps {
+  codeString: string;
+  language?: string;
+  theme?: string;
+  highlight: boolean;
+  gap: string;
+}
+
+/**
+ * 코드 컴포넌트
+ * @param {string} codeString - 보여줄 코드
+ * @param {string} language - 원하는 언어 선택 / default - 'tsx'
+ * @param {string} theme - 원하는 테마 / default -'github-dark'
+ *
+ * @example
+ * <Code codeString={codeString} language='tsx' theme='material-theme' />
+ */
+export default function Code({
+  codeString,
+  language = 'tsx',
+  theme = 'github-dark',
+  highlight = false, // 기본값 false
+  gap = 'gap-10', // 기본 gap 값
+}: CodeProps) {
+  const [highlightedCode, setHighlightedCode] = useState('');
+
+  useEffect(() => {
+    const loadHighlighter = async () => {
+      const html = await codeToHtml(codeString, {
+        lang: language, // 원하는 언어 선택 가능
+        theme: theme, // 원하는 테마로 변경 가능
+        decorations: [
+          {
+            start: codeString.indexOf(gap),
+            end: codeString.indexOf(gap) + gap.length,
+            properties: { class: 'highlighted-gap' }, // gap 강조
+          },
+        ],
+      });
+      setHighlightedCode(html);
+    };
+
+    loadHighlighter();
+  }, [codeString, language, theme, gap]);
+
+  return (
+    <div
+      className={`${highlight ? 'highlight-effect' : ''}`}
+      dangerouslySetInnerHTML={{ __html: highlightedCode }}
+    />
+  );
+}
