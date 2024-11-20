1e0b695c55af1a38890f2fe70f45b0f2c8cef21f
soheeyeo <sohee407@gmail.com>
2024-11-21 00:35:50 +0900

    ✨ [Feat] 동적 컴포넌트 매핑을 위한 CategoryComponents 객체 추가

---
+++ src/data/data.ts
@@ -0,0 +1,6 @@
+export const Data = [
+  { type: 'header', id: '1', title: 'Header 1' },
+  { type: 'header', id: '2', title: 'Header 2' },
+  { type: 'tab', id: '3', title: 'Tab 1' },
+  { type: 'tab', id: '4', title: 'Tab 2' },
+];
