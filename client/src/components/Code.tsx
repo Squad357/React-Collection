f2b64baff4fce647c37244dbc947bce725c2675c
soheeyeo <sohee407@gmail.com>
2025-01-02 14:38:48 +0900

    💄 [Styling] 모달 열기 버튼 추가

--- client/src/components/Code.tsx
+++ client/src/components/Code.tsx
@@ -4,6 +4,8 @@ import { useState } from 'react';
 import Modal from './layout/Modal';
 import { CodeProps } from '@/types/props/codeProps';
 import useShikiCode from '@/hooks/useShikiCode';
+import { HiCheck } from 'react-icons/hi';
+import useCopyCode from '@/hooks/useCopyCode';
 
 /**
  * 코드 컴포넌트
@@ -18,13 +20,29 @@ export default function Code({ codeString, optionList, animate }: CodeProps) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const highlightedCode = useShikiCode({ codeString, animate, optionList });
 
+  const { isCopied, handleCopyCode } = useCopyCode(highlightedCode);
+
   return (
     <>
+      <div className='flex gap-2 absolute top-2 right-5 text-sm '>
+        <button
+          className={`w-16 h-8 bg-[#24292e] text-white font-bold border rounded-full transition-all hover:bg-[#606060] ${
+            isCopied && 'bg-[#606060] cursor-default pointer-events-none'
+          }`}
+          onClick={handleCopyCode}>
+          {isCopied ? <HiCheck className='mx-auto w-6 h-6' /> : 'COPY'}
+        </button>
+        <button
+          className='w-16 h-8 bg-[#24292e] text-white font-bold border rounded-full transition-all hover:bg-[#606060]'
+          onClick={() => setIsModalOpen(true)}>
+          OPEN
+        </button>
+      </div>
       <div
-        className='w-full h-full overflow-auto rounded-lg pointer-events-auto cursor-pointer'
+        className='w-full h-full overflow-auto rounded-lg'
         dangerouslySetInnerHTML={{ __html: highlightedCode }}
-        onClick={() => setIsModalOpen(true)}
       />
+
       <Modal
         isModalOpen={isModalOpen}
         setIsModalOpen={setIsModalOpen}
