2c0768a184094b7e14eabddc8ef3a9c0de7141c2
soheeyeo <sohee407@gmail.com>
2025-01-02 14:32:50 +0900

    ➖ [Remove] 불필요한 파일 제거

--- client/src/lib/categoryComponents.ts
+++ client/src/lib/categoryComponents.ts
@@ -6,5 +6,4 @@ export const CategoryComponents: Record<string, ComponentType<PreviewProps>> = {
   1: dynamic(() => import('@/collections/headers/header1')),
   3: dynamic(() => import('@/collections/tabs/tab1')),
   4: dynamic(() => import('@/collections/tabs/tab2')),
-  5: dynamic(() => import('@/collections/tabs/tab3')),
 };
